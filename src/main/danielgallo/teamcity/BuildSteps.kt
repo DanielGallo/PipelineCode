08300a9b31ab0e85f5b09d989693f9e75c58f30a
danielgallo <daniel.gallo@gmail.com>
2023-08-04 18:37:43 +0100

    Sample build step

--- src/main/danielgallo/teamcity/BuildSteps.kt
+++ src/main/danielgallo/teamcity/BuildSteps.kt
@@ -1,11 +1,22 @@
 import jetbrains.buildServer.configs.kotlin.BuildSteps
 import jetbrains.buildServer.configs.kotlin.buildSteps.ScriptBuildStep
 
-fun BuildSteps.android(init: ScriptBuildStep.() -> Unit, apkName: String) : ScriptBuildStep {
-    val buildStep = ScriptBuildStep(init)
-    buildStep.scriptContent = """
+class AndroidBuildStep(
+    private val androidSdkPath: String,
+    private val apkName: String
+) : ScriptBuildStep ({
+    type = "android"
+    scriptContent = """
         echo "Building $apkName"
     """.trimIndent()
+})
 
-    return buildStep
+class AndroidBuildStepConfig {
+    var androidSdkPath: String = ""
+    var apkName: String = ""
+}
+
+fun BuildSteps.android(configure: AndroidBuildStepConfig.() -> Unit) : AndroidBuildStep {
+    val config = AndroidBuildStepConfig().apply(configure)
+    return AndroidBuildStep(config.androidSdkPath, config.apkName)
 }
