fc477aa39e6671b86c89cd2da3434bf4ab36a3a2
danielgallo <daniel.gallo@gmail.com>
2023-08-04 16:26:07 +0100

    Initial commit

---
+++ buildSteps/BuildSteps.kt
@@ -0,0 +1,11 @@
+package buildSteps
+
+import jetbrains.buildServer.configs.kotlin.buildSteps.ScriptBuildStep
+
+fun AndroidBuildStep(apkName : String) : ScriptBuildStep {
+    val buildStep = ScriptBuildStep {
+        scriptContent = """echo $apkName"""
+    }
+
+    return buildStep
+}
